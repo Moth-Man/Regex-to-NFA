c66350f2dd5c34c94d2d98c1e3b5ed78baa98920
Moth-Man <mothmanor@gmail.com>
2018-09-27 21:05:44 -0700

    Implementation of NFAStack, a class that houses the stack structure storing the NFA's.

--- NFAStack.java
+++ NFAStack.java
@@ -1,4 +1,57 @@
 
 public class NFAStack {
 
+	int maxSize;
+	int top;
+	NFA NFAarray[];
+	
+	public NFAStack(int n) {
+		maxSize = n;
+		NFAarray = new NFA[maxSize];
+		top = 0;
+		
+	}
+	
+	public boolean empty(){
+		
+		if(top == 0){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public void push(NFA nfa){
+		if(top < maxSize){
+			NFAarray[top] = nfa;
+			top++;
+		}
+		else {
+			System.out.println("Stack Overflow");
+		}
+	}
+	
+	public NFA pop(){
+		if(!this.empty()){
+			NFA temp = this.peek();
+			NFAarray[top-1] = null;
+			top--;
+			return temp;
+		}
+		else {
+			return null;
+		}
+		
+	}
+	
+	public NFA peek(){
+		if(top > 0){
+		 return NFAarray[top-1];
+		}
+		else {
+			return null;
+		}
+	}
+	
 }
