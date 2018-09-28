fd10ded9e4d8fda3abad4ad9de764652db8c102b
Moth-Man <mothmanor@gmail.com>
2018-09-27 23:04:30 -0700

    Create test compiler in main method to test various methods. Create stateGenerator method to create array of strings that represent the states of each NFA.

--- NFA.java
+++ NFA.java
@@ -5,7 +5,8 @@ public class NFA {
 	private String startState;
 	private String endState;
 	private int numStates;
-	private char[] link;
+	private char[] links;
+	private String[] states;
 	private TransitionFunction tranFunc;
 	private NFAStack stack = new NFAStack(1000);
 	
@@ -13,9 +14,10 @@ public class NFA {
 		this.startState = startState;
 		this.endState = endState;
 		this.numStates = numStates;
-		this.link = link;
+		this.links = links;
+		this.states = states;
 		this.stack = stack;
-		//this.tranFunc = tranFunc;
+		this.tranFunc = tranFunc;
 		
 	}
 	
@@ -52,14 +54,20 @@ public class NFA {
 		this.tranFunc = tranFunc;
 	}
 	
+	public String[] stateGenerator(NFA nfa){
+		int stNum = nfa.getNumStates();
+		for(int i = 0; i < stNum; i++){
+			states[i] = "q" + i;
+		}
+		return states;
+	}
 	//Creates NFA from single element of ArrayList of regexes
 	public void analyzeNFA(String regex){
 		int length = regex.length();
 		for(int i = 0; i < length; i++){
 			char c = regex.charAt(i);
 			if(c == 'a' || c == 'b' || c == 'c' || c == 'd' || c == 'e'){
-				
-				NFA nFAc = createCharNFA();
+				NFA nFAc = createCharNFA(c);
 				stack.push(nFAc);
 			}
 			else if(c == '&'){
@@ -82,12 +90,12 @@ public class NFA {
 	public NFA createCharNFA(char c){
 		char[] ch = new char[1];
 		ch[0] = c;
-		NFA charNFA = new NFA("q1", "q2", 2, ch[]);
+		NFA charNFA = new NFA("q1", "q2", 2, ch);
 		return charNFA;
 	}
 	
-	public NFA createUnionNFA(){
-		
+	public NFA createUnionNFA(NFA nFA1, NFA nFA2){
+		NFA unionNFA = new NFA();
 	}
 	
 	public NFA createConcatNFA(NFA nFA1, NFA nFA2){
--- RegExCompiler.java
+++ RegExCompiler.java
@@ -3,11 +3,21 @@ import java.io.File;
 
 public class RegExCompiler {
 
+	/*
 	public static void main(String args[]){
 		if(args.length > 0){
 			File file =  new File(args[0]);
 			RegExScanner scan = new RegExScanner();
 			ArrayList<String> regEx = scan.scanRegexFile(file);
 		}
+	}
+	
+	*/
+	
+	//Test compiler
+	public static void main(String args[]) {
+		char[] ch = new char[3];
+		NFA nfa = new NFA("Q1", "Q2", 3, ch);
+		nfa.stateGenerator(nfa);
 	}
 }
