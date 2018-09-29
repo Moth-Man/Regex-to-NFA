0b5cb71769ff2ebc0c8bab3206e48924086512c7
Moth-Man <mothmanor@gmail.com>
2018-09-29 01:27:35 -0700

    Change stateGenerator method to return array of int instead of array of strings. This way, the start state and end state can be int's as well that can be manipulated within Transition Function graph.

--- NFA.java
+++ NFA.java
@@ -6,7 +6,7 @@ public class NFA {
 	private String endState;
 	private int numStates;
 	private char[] links;
-	private State[] states = new String[numStates];
+	private int[] states = new int[numStates];
 	private TransitionFunction tranFunc;
 	private NFAStack stack = new NFAStack(1000);
 	
@@ -54,11 +54,11 @@ public class NFA {
 		this.tranFunc = tranFunc;
 	}
 	
-	public String[] getStates() {
+	public int[] getStates() {
 		return states;
 	}
 	
-	public void setStates(String[] states){
+	public void setStates(int[] states){
 		this.states = states;
 	}
 	
@@ -71,13 +71,13 @@ public class NFA {
 	}
 	
 	
-	public String[] stateGenerator(NFA nfa){
+	public int[] stateGenerator(NFA nfa){
 		int stNum = nfa.getNumStates();
-		String[] st8 = new String[stNum];
+		int[] st8 = new int[stNum];
 		nfa.setStates(st8);
 		for(int i = 0; i < stNum; i++){
 			String str = "q" + Integer.toString(i);
-			st8[i] = str;
+			st8[i] = i;
 			System.out.println(str);
 		}
 		return states;
@@ -108,7 +108,8 @@ public class NFA {
 		}
 		NFA finalNFA = stack.pop();
 		System.out.println(regex);
-		tranFunc.generateTransitionFunction(finalNFA);
+		tranFunc = finalNFA.getTranFunc();
+		tranFunc.generateTransitionFunction();
 	}
 	
 	public NFA createCharNFA(char c){
@@ -118,8 +119,11 @@ public class NFA {
 		String[] st8 = charNFA.stateGenerator(charNFA);
 		charNFA.setStartState(st8[0]);
 		charNFA.setEndState(st8[st8.length-1]);
-		System.out.println(charNFA.getStartState() + charNFA.getEndState());
-		System.out.println("(" + charNFA.getStartState() + "," + c + ") --> " + charNFA.getEndState());
+		
+		TransitionFunction tf = new TransitionFunction(charNFA.getNumStates());
+		tf.addEdge(st8[0], st8[], link);
+		//System.out.println(charNFA.getStartState() + charNFA.getEndState());
+		//System.out.println("(" + charNFA.getStartState() + "," + c + ") --> " + charNFA.getEndState());
 		
 		return charNFA;
 	}
